7dcff2af7c5a07f5f8614116aa7c218d8f5fdd7c
nulysses <adr.zivil@gmail.com>
2021-05-16 11:01:52 +0200

    1

--- install.sh
+++ install.sh
@@ -12,12 +12,13 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 . "${script_dir}/vars"
 
+code_search='for the_file in "~/.config/ar18/autostarts/"\*; do "${the_file}"; done'
 code='for the_file in "~/.config/ar18/autostarts/"*; do "${the_file}"; done'
 set +e
-content="$(cat "/home/${user_name}/.xprofile" | grep "${code}")"
+content="$(cat "/home/${user_name}/.xprofile" | grep "${code_search}")"
 set -e
-if [[ "${content}" == "" ]]; then
-  echo "${code}" >> "/home/${user_name}/.xprofile"
+if [ "${content}" = "" ]; then
+  echo -e "\n${code}" >> "/home/${user_name}/.xprofile"
 fi
 
 mkdir -p "/home/${user_name}/.config/ar18/autostarts"
