5ddc336441a09a74fd33de35f8c7c0092f0a418a
nulysses <adr.zivil@gmail.com>
2021-06-13 00:55:16 +0200

    1

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ar18
+# ar18#
 
 # Script template version 2021-06-13
 # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
@@ -49,6 +49,7 @@ if [ "${content}" = "" ]; then
 fi
 
 mkdir -p "/home/${user_name}/.config/ar18/autostarts"
+
 ##################################SCRIPT_END###################################
 # Restore old shell values
 set +x
